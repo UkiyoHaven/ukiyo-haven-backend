d6e2e2366d4c494b7664a9a6ef03d3848cd3a944
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-15 16:10:07 +0100

    chore: update imports (again)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
