669f0656fd3ab6f757583a4e754a7e30f7804473
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-15 16:13:54 +0100

    fix: missing test module import

--- src/journal/journal.service.spec.ts
+++ src/journal/journal.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JournalService } from './journal.service';
+import { PrismaService } from '../prisma/prisma.service'
 
 describe('JournalService', () => {
   let service: JournalService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [JournalService],
+      providers: [JournalService, PrismaService],
     }).compile();
 
     service = module.get<JournalService>(JournalService);
