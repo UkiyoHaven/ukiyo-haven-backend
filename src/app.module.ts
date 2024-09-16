8cec29e6b61a35da596a27b9895c9e49a7c89f03
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-16 15:02:20 +0100

    chore: Add the GoalService and GoalController to the AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,12 @@ import { JournalController } from './journal/journal.controller';
 import { JournalService } from './journal/journal.service';
 import { ConfigModule } from '@nestjs/config';
 import { DiscussionsGateway } from './discussions/discussions.gateway';
+import { GoalController } from './goal/goal.controller';
+import { GoalService } from './goal/goal.service';
+
 @Module({
   imports: [AuthModule, PrismaModule, ConfigModule.forRoot()],
-  controllers: [AppController, JournalController],
-  providers: [AppService, JournalService, DiscussionsGateway],
+  controllers: [AppController, JournalController, GoalController],
+  providers: [AppService, JournalService, DiscussionsGateway, GoalService],
 })
 export class AppModule {}
