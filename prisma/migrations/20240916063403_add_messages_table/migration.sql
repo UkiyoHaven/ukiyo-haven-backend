40aa1be7947dd0b19644e066dc538361a881a7be
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-16 07:35:36 +0100

    Feat: Added message persistence in Prisma database, saving messages from the real-time discussion board.

---
+++ prisma/migrations/20240916063403_add_messages_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "user" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
