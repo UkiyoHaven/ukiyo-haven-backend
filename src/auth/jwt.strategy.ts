0a89357d90fd7522b24455fb23af950b8321424c
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-16 15:21:04 +0100

    Feat: Add error logging to JWTStrategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from '../prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService, private prisma: PrismaService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Extract JWT from Bearer token
-      ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET'),  // JWT secret from .env
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      //ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
   async validate(payload: any) {
+    console.log('Validating JWT with payload:', payload);
     const user = await this.prisma.user.findUnique({ where: { id: payload.sub } });
+
     if (!user) {
+      console.error('User not found for ID:', payload.sub);
       throw new Error('User not found');
     }
+
     return user;  // Attach the user to the request object
   }
 }
