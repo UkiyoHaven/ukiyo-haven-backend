44269b73c3fae6a37b5a33254b03dcd0a0430923
Ricky Ultimate <rickyogigi@gmail.com>
2024-09-17 05:56:22 +0100

    Feat: comment out JWT validation log as it floods the console

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -15,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    console.log('Validating JWT with payload:', payload);
+    //console.log('Validating JWT with payload:', payload);
     const user = await this.prisma.user.findUnique({ where: { id: payload.sub } });
 
     if (!user) {
